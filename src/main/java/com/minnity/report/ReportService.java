c94be5abe93f309dcd74b1ae813661aa30e1a5f9
Milk T <milkt2021@gmail.com>
2024-01-17 18:59:38 +0100

    commiting changes

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -13,23 +13,6 @@ public class ReportService {
 
     //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
     public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs)  {
-       /* Map<Integer, Long> numberOfRequestsMadeToCompany = new HashMap<>();
-
-        for (RequestLog requestLog : requestLogs) {
-            Integer companyId = requestLog.getCompanyId();
-            Long totalNumberOfRequestsPerCompany = requestLogs.stream()
-                    .filter(rl -> rl.getCompanyId() == companyId)
-                    .filter(rl -> (rl.getRequestMethod().equals("PUT")
-                            || rl.getRequestMethod().equals("GET")
-                            || rl.getRequestMethod().equals("POST"))).count();
-
-            numberOfRequestsMadeToCompany.put(companyId, totalNumberOfRequestsPerCompany);
-        }
-
-        if (numberOfRequestsMadeToCompany.size() >= 1)
-            return numberOfRequestsMadeToCompany;
-        throw new RequestNotFoundException("No request matching condition is found");*/
-
 
         return requestLogs.stream()
                 .filter(rl -> (rl.getRequestMethod().equals("PUT")
@@ -40,23 +23,6 @@ public class ReportService {
 
     //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
     public Map<Integer, List<RequestLog>> findRequestsWithError(List<RequestLog> requestLogs) {
-        /*Changed method return parameter to Map<Integer,  List<RequestLog>>  to match companyId with requests per
-          company that finished with an error HTTP response code (>=400)*/
-       /* Map<Integer, List<RequestLog>> requestsWithError = new HashMap<>();
-
-        for (RequestLog requestLog : requestLogs) {
-            Integer companyId = requestLog.getCompanyId();
-            List<RequestLog> reqLog = requestLogs.stream()
-                    .filter(rl -> rl.getCompanyId() == companyId)
-                    .filter(rl -> rl.getRequestStatus() >= 400).collect(Collectors.toList());
-
-            requestsWithError.put(companyId, reqLog);
-        }
-
-        if (requestsWithError.size() >= 1)
-            return requestsWithError;
-        throw new RequestNotFoundException("No request matching condition is found");*/
-
         return requestLogs.stream()
                 .filter(rl -> rl.getRequestStatus() >= 400)
                 .collect(groupingBy(RequestLog::getCompanyId, Collectors.toList()));
